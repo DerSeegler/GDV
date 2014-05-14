71ee7f13a68e4922ba64ce05877c9f2e4b17c656
Yannik S. <yannikseegel@aol.com>
2014-05-14 20:08:20 +0200

    Added lighting

--- mySwing/mySwing.cpp
+++ mySwing/mySwing.cpp
@@ -5,7 +5,6 @@
 
 #include <iostream> 
 #include <GL/freeglut.h>         //lädt alles für OpenGL
-#include "wuerfel.h"
 #include <math.h>
 
 //using namespace std;
@@ -15,13 +14,25 @@ float winkelGeschwindigekeit = 0.0;
 float winkel = 0.0;
 float winkel2 = 0.0;
 float x,y,z = 0.0;
+int zylinderGenauigkeit = 100;
 
 void Init()	
 {
    // Hier finden jene Aktionen statt, die zum Programmstart einmalig 
    // durchgeführt werden müssen
-	glEnable(GL_DEPTH_TEST);
-	glClearDepth(1.0);
+	glClearColor(1.0, 0.6, 0.0, 1.0);
+
+	// Licht
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    GLfloat light_position [] = {1.0, 1.0, 1., 0.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position ); // Licht Nr. 0 rechts oben
+    glEnable(GL_COLOR_MATERIAL);
+    // z-Buffer
+    glEnable(GL_DEPTH_TEST);
+    glClearDepth(1.0);
+    // Normalen fuer korrekte Beleuchtungs-Berechnung normalisieren
+    glEnable(GL_NORMALIZE);
 
 }
 
@@ -67,6 +78,7 @@ void Zylinder(float height, float resolution, float radius, float color [4])
 void Rotorblaetter()
 {
 	glBegin(GL_POLYGON);
+	glNormal3f(0., 0., -1);
 	glVertex3f(0, 0, 0);
 	glVertex3f(-1, 2, 0);
 	glVertex3f(-0.45, 3, 0);
@@ -77,17 +89,25 @@ void Rotorblaetter()
 	glBegin(GL_QUAD_STRIP);
 	glVertex3f(0, 0, 0);
 	glVertex3f(0, 0, 0.1);
+	glNormal3f(-0.2, -0.1, 0);
 	glVertex3f(-1, 2, 0);
 	glVertex3f(-1, 2, 0.1);
+	glNormal3f(1., -0.55, 0);
 	glVertex3f(-0.45, 3, 0);
 	glVertex3f(-0.45, 3, 0.1);
+	glNormal3f(0., 1., 0);
 	glVertex3f(0.45, 3, 0);
 	glVertex3f(0.45, 3, 0.1);
+	glNormal3f(1., 0.55, 0);
 	glVertex3f(1, 2, 0);
 	glVertex3f(1, 2, 0.1);
+	glNormal3f(-0.2, 0.1, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 0.1);
 	glEnd();
 	
 	glBegin(GL_POLYGON);
+	glNormal3f(0., 0., 1);
 	glVertex3f(0, 0, 0.1);
 	glVertex3f(-1, 2, 0.1);
 	glVertex3f(-0.45, 3, 0.1);
@@ -102,9 +122,9 @@ void RenderScene() //Zeichenfunktion
    // Hier befindet sich der Code der in jedem Frame ausgefuehrt werden muss
    glLoadIdentity ();   // Aktuelle Model-/View-Transformations-Matrix zuruecksetzen
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   glClearColor(1.0, 0.6, 0.0, 1.0);
+   //glClearColor(1.0, 0.6, 0.0, 1.0);
    //glTranslatef(0., 0., -1);
-   gluLookAt(0.+x, 0.+y, 6.+z, 0., 0., 0., 0., 1., 0.);
+   gluLookAt(0.+x, 1.+y, 6.+z, 0., 0., 0., 0., 1., 0.);
 
    float braun [4] = { 0.4, 0.3, 0.3, 1. };
    float weiss[4] = { 1, 1, 1, 1. };
@@ -114,37 +134,46 @@ void RenderScene() //Zeichenfunktion
 
    // Tragestange
    glPushMatrix();
-   glTranslatef(1.25, 2., 0.);
-   glRotatef(90, 0., 0., 1.);
-   Zylinder(2.5, 1000, 0.05, silber);
+   glTranslatef(-1.25, 2., 0.);
+   glRotatef(90, 0., 1., 0.);
+   glColor4f(0.75, 0.75, 0.75, 1.);
+   glutSolidCylinder(0.05, 2.5, zylinderGenauigkeit, zylinderGenauigkeit);;
    glPopMatrix();
 
    // Standfuss oben links
    glPushMatrix();
    glTranslatef(-1.2, -0.584, -0.941);
    glRotatef(20, 1., 0., 0.);
-   Zylinder(2.75, 1000, 0.05, silber);
+   glRotatef(90, -1., 0., 0.);
+   glColor4f(0.75, 0.75, 0.75, 1.);
+   glutSolidCylinder(0.05, 2.75, zylinderGenauigkeit, zylinderGenauigkeit);;
    glPopMatrix();
 
    // Standfuss unten links
    glPushMatrix();
    glTranslatef(-1.2, -0.584, 0.941);
    glRotatef(-20, 1., 0., 0.);
-   Zylinder(2.75, 1000, 0.05, silber);
+   glRotatef(90, -1., 0., 0.);
+   glColor4f(0.75, 0.75, 0.75, 1.);
+   glutSolidCylinder(0.05, 2.75, zylinderGenauigkeit, zylinderGenauigkeit);;
    glPopMatrix();
 
    // Standfuss oben rechts
    glPushMatrix();
    glTranslatef(1.2, -0.584, -0.941);
    glRotatef(20, 1., 0., 0.);
-   Zylinder(2.75, 1000, 0.05, silber);
+   glRotatef(90, -1., 0., 0.);
+   glColor4f(0.75, 0.75, 0.75, 1.);
+   glutSolidCylinder(0.05, 2.75, zylinderGenauigkeit, zylinderGenauigkeit);;
    glPopMatrix();
 
    // Standfuss unten rechts
    glPushMatrix();
    glTranslatef(1.2, -0.584, 0.941);
    glRotatef(-20, 1., 0., 0.);
-   Zylinder(2.75, 1000, 0.05, silber);
+   glRotatef(90, -1., 0., 0.);
+   glColor4f(0.75, 0.75, 0.75, 1.);
+   glutSolidCylinder(0.05, 2.75, zylinderGenauigkeit, zylinderGenauigkeit);;
    glPopMatrix();
 
    glTranslatef(0, 2.0175, 0);
@@ -154,45 +183,54 @@ void RenderScene() //Zeichenfunktion
    // Sitzfläche
    glPushMatrix();
    glScalef(0.75, 0.025, 0.25);
-   Wuerfel(1, weiss);
+   glColor4f(1.,1.,1.,1.);	
+   glutSolidCube(1);
    glPopMatrix();
 
    // Lehne
    glPushMatrix();
    glTranslatef(0, 0.05, -0.15);
    glScalef(0.05, 0.125, 0.05);
-   Wuerfel(1, rot);
+   glColor4f(1., 0., 0., 1.);
+   glutSolidCube(1); // rot
    glPopMatrix();
    glPushMatrix();
    glTranslatef(0, 0.2375, -0.15);
    glScalef(0.6, 0.25, 0.02);
-   Wuerfel(1, blau);
+   glColor4f(0., 0., 1., 1.);
+   glutSolidCube(1); // blau
    glPopMatrix();
 
    // Seilbefestigung links
    glPushMatrix();
    glTranslatef(-0.375, 0., 0.);
-   glRotatef(90, 0., 0., 1.);
-   Zylinder(0.1, 1000, 0.035, braun);
+   glRotatef(90, 0., -1., 0.);
+   glColor4f(0.4, 0.3, 0.3, 1.);
+   glutSolidCylinder(0.035, 0.1, zylinderGenauigkeit, zylinderGenauigkeit);
    glPopMatrix();
 
    // Seilbefestigung rechts
    glPushMatrix();
    glTranslatef(0.475, 0., 0.);
-   glRotatef(90, 0., 0., 1.);
-   Zylinder(0.1, 1000, 0.035, braun);
+   glRotatef(90, 0., -1., 0.);
+   glColor4f(0.4, 0.3, 0.3, 1.);
+   glutSolidCylinder(0.035, 0.1, zylinderGenauigkeit, zylinderGenauigkeit);
    glPopMatrix();
 
    // Seil links
    glPushMatrix();
    glTranslatef(0.425, 0., 0.);
-   Zylinder(2., 1000, 0.02, blau);
+   glRotatef(90, -1., 0., 0.);
+   glColor4f(0, 0, 1, 1.);
+   glutSolidCylinder(0.02, 2, zylinderGenauigkeit, zylinderGenauigkeit);
    glPopMatrix();
 
    // Seil rechts
    glPushMatrix();
    glTranslatef(-0.425, 0., 0.);
-   Zylinder(2., 1000, 0.02, blau);
+   glRotatef(90, -1., 0., 0.);
+   glColor4f(0, 0, 1, 1.);
+   glutSolidCylinder(0.02, 2, zylinderGenauigkeit, zylinderGenauigkeit);
    glPopMatrix();
 
    glTranslatef(0, 0.2365, -0.185);
@@ -202,8 +240,9 @@ void RenderScene() //Zeichenfunktion
    // Ventilator Befestigung
    glPushMatrix();
    glTranslatef(0, 0.2375, -0.17);
-   glRotatef(90, -1., 0., 0.);
-   Zylinder(0.1, 1000, 0.05, braun);
+   glRotatef(180, 0., 1., 0.);
+   glColor4f(0.4, 0.3, 0.3, 1.);
+   glutSolidCylinder(0.05, 0.1, zylinderGenauigkeit, zylinderGenauigkeit);
    glPopMatrix();
 
    glPushMatrix();
@@ -267,8 +306,6 @@ void Animate (int value)
   // std::cout << "value=" << value << std::endl;
    // RenderScene aufrufen
    glutPostRedisplay();
-
-   
   
    fRotation = fRotation - 0.1;  // Rotationswinkel aendern 
    if (fRotation <= 0.0) {
@@ -286,7 +323,7 @@ void Animate (int value)
 
   // std::cout << fRotation << std::endl;
    // Timer wieder registrieren; Animate wird so nach 100 msec mit value+=1 aufgerufen
-   glutTimerFunc(5, Animate, ++value);          
+   glutTimerFunc(50, Animate, ++value);          
 }
 
 void KeyboardFunc ( unsigned char key, int x, int y )
